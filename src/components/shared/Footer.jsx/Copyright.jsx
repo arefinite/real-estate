231a653958aa519571e95a2af917a1660987d054
arefinite <arefinite@gmail.com>
2023-12-17 18:08:53 +0600

    privacy added

--- src/components/shared/Footer.jsx/Copyright.jsx
+++ src/components/shared/Footer.jsx/Copyright.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Copyright = () => {
 	return (
-		<div className='text-stone-400 px-4 pb-20 flex justify-center items-center'>
-			Copyright &copy; 2023. All Rights Reserved.
-		</div>
+		<>
+			<div className='text-stone-400 px-4 flex pb-2 justify-center items-center'>
+				Copyright &copy; 2023. All Rights Reserved.
+			</div>
+			<div className='text-stone-500 text-sm px-4 pb-20 flex justify-center items-center'>
+				<Link to='privacy-policy'>Privacy & Policy</Link>
+			</div>
+		</>
 	)
 }
 
